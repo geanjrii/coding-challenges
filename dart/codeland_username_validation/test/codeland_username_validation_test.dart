bb73710db57046799b497156e0f02439a9f8cb7f
Gean jr. <gean.junior@academico.ufpb.br>
2025-09-20 23:20:06 -0300

    test(codeland username validation/dart): add more tests

--- dart/codeland_username_validation/test/codeland_username_validation_test.dart
+++ dart/codeland_username_validation/test/codeland_username_validation_test.dart
@@ -9,4 +9,42 @@ void main() {
   test('Valid username with multiple underscores', () {
     expect(codelandUsernameValidation("u__hello_world123"), equals("true"));
   });
+
+  test('Valid username: only letters, length 10', () {
+    expect(codelandUsernameValidation("aaaaaaaaaa"), equals("true"));
+  });
+
+  test('Single underscore', () {
+    expect(codelandUsernameValidation("_"), equals("false"));
+  });
+
+  test('Starts with underscores', () {
+    expect(codelandUsernameValidation("__bbbbbbb"), equals("false"));
+  });
+
+  test('Ends with underscore', () {
+    expect(codelandUsernameValidation("b3333434_"), equals("false"));
+  });
+
+  test('Valid username: letters and digits', () {
+    expect(codelandUsernameValidation("usernamehello123"), equals("true"));
+  });
+
+  test('Too many underscores, length > 25', () {
+    expect(
+      codelandUsernameValidation("oooooooooooooooooo________a"),
+      equals("false"),
+    );
+  });
+
+  test('Starts with digit', () {
+    expect(codelandUsernameValidation("123abc444"), equals("false"));
+  });
+
+  test('Too long, valid chars', () {
+    expect(
+      codelandUsernameValidation("a______b_________555555555555aaaa"),
+      equals("false"),
+    );
+  });
 }
