b788eb5290ed304d1cdc50d9db507170e468532d
Gean jr. <gean.junior@academico.ufpb.br>
2025-09-20 23:11:14 -0300

    test(longest word/dart): add more tests

--- dart/longest_word/test/longest_word_test.dart
+++ dart/longest_word/test/longest_word_test.dart
@@ -9,4 +9,46 @@ void main() {
   test('returns longest word from a simple sentence', () {
     expect(longestWord("I love dogs"), equals("love"));
   });
+
+  test('returns longest word for "hello world"', () {
+    expect(longestWord("hello world"), equals("hello"));
+  });
+
+  test('returns longest word for "this is some sort of sentence"', () {
+    expect(longestWord("this is some sort of sentence"), equals("sentence"));
+  });
+
+  test('returns longest word for "longest word!!"', () {
+    expect(longestWord("longest word!!"), equals("longest"));
+  });
+
+  test('returns longest word for "a beautiful sentence^&!"', () {
+    expect(longestWord("a beautiful sentence^&!"), equals("beautiful"));
+  });
+
+  test('returns longest word for "oxford press"', () {
+    expect(longestWord("oxford press"), equals("oxford"));
+  });
+
+  test('returns longest word for "123456789 98765432"', () {
+    expect(longestWord("123456789 98765432"), equals("123456789"));
+  });
+
+  test('returns longest word for "letter after letter!!"', () {
+    expect(longestWord("letter after letter!!"), equals("letter"));
+  });
+
+  test('returns longest word for "a b c dee"', () {
+    expect(longestWord("a b c dee"), equals("dee"));
+  });
+
+  test(
+    'returns longest word for "a confusing /:sentence:/[ this is not!!!!!!!~"',
+    () {
+      expect(
+        longestWord("a confusing /:sentence:/[ this is not!!!!!!!~"),
+        equals("confusing"),
+      );
+    },
+  );
 }
