42553682e79a110c3a1199a1702005cad94a0d6e
Gean jr. <gean.junior@academico.ufpb.br>
2025-09-20 22:05:23 -0300

    docs(first reverse/dart): add more tests

--- dart/first_reverse/test/first_reverse_test.dart
+++ dart/first_reverse/test/first_reverse_test.dart
@@ -6,8 +6,40 @@ void main() {
     expect(firstReverse("coderbyte"), "etybredoc");
   });
 
-  test('should reverse "I Love Code"', () {
-    expect(firstReverse("I Love Code"), "edoC evoL I");
+  test('should reverse "Coderbyte"', () {
+    expect(firstReverse("Coderbyte"), "etybredoC");
+  });
+
+  test('should reverse "I Love Coding"', () {
+    expect(firstReverse("I Love Coding"), "gnidoC evoL I");
+  });
+
+  test('should reverse "h333llLo"', () {
+    expect(firstReverse("h333llLo"), "oLll333h");
+  });
+
+  test('should reverse "Yo0"', () {
+    expect(firstReverse("Yo0"), "0oY");
+  });
+
+  test('should reverse "thisiscool"', () {
+    expect(firstReverse("thisiscool"), "loocsisiht");
+  });
+
+  test('should reverse "commacomma!"', () {
+    expect(firstReverse("commacomma!"), "!ammocammoc");
+  });
+
+  test('should reverse "123456789"', () {
+    expect(firstReverse("123456789"), "987654321");
+  });
+
+  test('should reverse "lettersz!23z"', () {
+    expect(firstReverse("lettersz!23z"), "z32!zsrettel");
+  });
+
+  test('should reverse "aq"', () {
+    expect(firstReverse("aq"), "qa");
   });
 
   test('should reverse "Hello World and Coders"', () {
